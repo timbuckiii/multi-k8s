ce9e6960ca25e30a8e4d26e7e2d45c41c35aa388
Nathan Devey <nathan@clearbanc.com>
2020-12-30 18:14:34 -0500

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker push timbuckiii/multi-client:$SHA
 docker push timbuckiii/multi-server:$SHA
 docker push timbuckiii/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image depoyments/server-deployment server=timbuckiii/multi-server:$SHA
-kubectl set image depoyments/client-deployment client=timbuckiii/multi-client:$SHA
-kubectl set image depoyments/worker-deployment server=timbuckiii/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=timbuckiii/multi-server:$SHA
+kubectl set image deployments/client-deployment client=timbuckiii/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=timbuckiii/multi-worker:$SHA
